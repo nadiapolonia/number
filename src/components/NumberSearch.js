c2d1a8e9496fd86c866e38d88842260a9e0624ee
Nadia Murchison <nadiamurchison@gmail.com>
2019-08-29 15:14:59 -0400

    bugs fixed- thanks mark!

--- src/components/NumberSearch.js
+++ src/components/NumberSearch.js
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-// List of Bugs:
-
-// Search will only submit the first digit of the number entered. Numbers following the first digit are not recognized and an error message is created.
-
-// When the search box is empty (no number) a huge string of  API html shows.
-
-// Message will pull multiple results from the number entered and change through them before finding a final result.
-
 // API website: http://numbersapi.com/#42
 
 const NumberSearch = () => {
-  const [numberSearch, setNumberSearch] = useState()
+  const [numberSearch, setNumberSearch] = useState('')
   const [numMessage, setNumMessage] = useState('')
   //   const [numberMessage, setNumberMessage] = useState('')
 
   const fetchData = async () => {
-    const resp = await axios.get(`http://numbersapi.com/${numberSearch}/`)
+    const resp = await axios.get(`http://numbersapi.com/${numberSearch}/trivia`)
 
     console.log(resp)
     console.log(resp.data)
@@ -26,30 +18,25 @@ const NumberSearch = () => {
     setNumMessage(message)
   }
 
-  useEffect(() => {
-    fetchData()
-  })
-
   return (
     <>
       <main>
         <h3>Find out a random fact related to the number you enter!</h3>
         <section>
-          {/* <form
+          <form
             onSubmit={e => {
               e.preventDefault()
-              //   setNumMessage(fetchData)
-              setNumberSearch(e.target.value)
+              fetchData()
             }}
-          > */}
-          <input
-            type="text"
-            placeholder="enter number here"
-            value={numberSearch}
-            onChange={e => setNumberSearch(e.target.value)}
-          />
-          {/* </form> */}
-          {/* <button onClick={e => setNumberSearch(e.target.value)}>Submit</button> */}
+          >
+            <input
+              type="text"
+              placeholder="enter number here"
+              value={numberSearch}
+              onChange={e => setNumberSearch(e.target.value)}
+            />
+          </form>
+          <button>Submit</button>
         </section>
 
         <p className="random">Random fact:</p>
